0350067890bc2ab8f7e8378fc1772b80a9e78c28
rajan2rajan <rajanbhandari939@gmail.com>
2022-01-30 02:14:48 +0545

    changing

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,54 @@
-
-pub mod Processor;
-pub mod error;
 pub mod utils;
-use crate::{
-    processor::Processor,
-    error::TokenError
-};
-use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult,
-    pubkey::Pubkey,
-    program_error::PrintProgramError,
-};
 
-pub const PREFIX:&str="withdraw_sol";
+use borsh::{BorshDeserialize, BorshSerialize};
+use error::{ProgramError, TokenError};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    decode_error::DecodeError,
+    entrypoint,
+    entrypoint::ProgramResult,
+    instruction, msg,
+    program::{invoke, invoke_signed},
+    program_error::{PrintProgramError, ProgramError},
+    pubkey::Pubkey,
+    system_program,
+    sysvar::{clock::Clock, fees::Fees, rent::Rent, Sysvar},
+};
+pub struct SendingAccount {
+    amount: u64,
+}
+
+pub const PREFIX: &str = "withdraw_sol";
 entrypoint!(process_instruction);
 fn process_instruction(
-    program_id: &Pubkey, // Public key of the account the hello world program was loaded into
-    accounts: &[AccountInfo], // The account to say hello to
-    _instruction_data: &[u8],
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
 ) -> ProgramResult {
-    if let Err(error) = Processor::process(program_id, accounts, data) {
-        // catch the error so we can print it
-        error.print::<TokenError>();
-        return Err(error);
+    msg!("hellow world");
+    let account_info_iter = &mut accounts.iter();
+    let source_account_info = next_account_info(account_info_iter)?; // source account
+    let dest_account_info = next_account_info(account_info_iter)?; // dest account
+    let lamport = next_account_info(account_info_iter)?; // lamport send
+    let send_data = next_account_info(account_info_iter)?; // program send data
+    let system_program = next_account_info(account_info_iter)?; //system program (used to creat account)
+
+    if !source_account_info.is_signer {
+        return Err(ProgramError::IncorrectProgramId);
     }
+    if lamport.data_is_empty() {
+        return Err(ProgramError::InsufficientFunds);
+    }
+
+    let rent = Rent::get()?; //rent to be paid to store data.
+    if send_data.data_is_empty() {
+        // is empty vanpaxi afi new function creat garna parxa. yo function la account info mai define garako xa
+        return Err(TokenError::NoDataToSend);
+    }
+
+    let mut sending_account = SendingAccount::try_from_slice(&accounts.data.borrow())?;
+
+    sending_account.serialize(&mut &mut accounts.data.borrow_mut()[..])?;
+
     Ok(())
 }
--- src/utils.rs
+++ src/utils.rs
@@ -2,6 +2,7 @@ use crate::{
     account_info::AccountInfo, entrypoint::ProgramResult, instruction::Instruction, pubkey::Pubkey;
     solana_sdk::system_instruction::SystemInstruction;
 };
+pub mod error;
 pub fn transfer(from_pubkey: &Pubkey, to_pubkey: &Pubkey, lamports: u64) -> Instruction {
     let account_metas = vec![
         AccountMeta::new(*from_pubkey, true),
@@ -13,6 +14,16 @@ pub fn transfer(from_pubkey: &Pubkey, to_pubkey: &Pubkey, lamports: u64) -> Inst
         account_metas,
     )
 }
+
+fn try_from_slice(v: &[u8]) -> Result<Self> {
+    let mut v_mut = v;
+    let result = Self::deserialize(&mut v_mut)?;
+    if !v_mut.is_empty() {
+        return Err(Error::new(ErrorKind::InvalidData, ERROR_NOT_ALL_BYTES_READ));
+    }
+    Ok(result)
+}
+
 
 pub fn create_transfer_unsigned<'a>(
     sender: &AccountInfo<'a>,
